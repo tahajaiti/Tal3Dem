11c094556391f56e07928c0da9e77ea5f6a83140
tahajaiti <taha.jaiti@gmail.com>
2025-10-08 14:39:22 +0100

    FEAT: DM-29 created the inject and injectable annotations and the enum for choosing the scope

---
+++ src/main/java/org/kyojin/core/Injectable.java
@@ -0,0 +1,17 @@
+package org.kyojin.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Injectable {
+    enum Scope {
+        SINGLETON,
+        FACTORY
+    }
+
+    Scope scope() default Scope.SINGLETON;
+}
