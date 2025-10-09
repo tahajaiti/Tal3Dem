17f45ccf1085017303ce3bfd63a513ad0de03f88
tahajaiti <taha.jaiti@gmail.com>
2025-10-09 12:02:19 +0100

    FEAT: DM-35 created the entity manager provider for handling the database connection

---
+++ src/main/java/org/kyojin/provider/EntityManagerProvider.java
@@ -0,0 +1,27 @@
+package org.kyojin.provider;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.kyojin.core.Injectable;
+
+@Injectable
+public class EntityManagerProvider {
+
+    private final EntityManagerFactory emf;
+
+    public EntityManagerProvider() {
+        this.emf = Persistence.createEntityManagerFactory("tal3demPU");
+    }
+
+    public EntityManager get() {
+        return emf.createEntityManager();
+    }
+
+    public void close() {
+        if (emf.isOpen()) {
+            emf.close();
+        }
+    }
+
+}
