a4bcff4473c523ff8189c51e2ecf483b39ab55fd
tahajaiti <taha.jaiti@gmail.com>
2025-10-09 14:28:38 +0100

    FEAT: DM-34 created the interface repository for medical profile and receiver entity

---
+++ src/main/java/org/kyojin/repository/MedicalProfileRepository.java
@@ -0,0 +1,6 @@
+package org.kyojin.repository;
+
+import org.kyojin.entity.MedicalProfile;
+
+public interface MedicalProfileRepository extends Repository<MedicalProfile, Long> {
+}
---
+++ src/main/java/org/kyojin/repository/ReceiverRepository.java
@@ -0,0 +1,6 @@
+package org.kyojin.repository;
+
+import org.kyojin.entity.Receiver;
+
+public interface ReceiverRepository extends Repository<Receiver, Long>{
+}
